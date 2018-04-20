22fcc920cb7a5cf75006ebd62f7ba812d1e43f05
Ilya Shusterman <ilya@allaspect.com>
2018-04-20 05:52:59 +0300

    added file saver storage at google cloud

--- firebase_storage.py
+++ firebase_storage.py
@@ -1,14 +1,28 @@
+import os
 from unittest import TestCase
-
+from google.cloud import storage
 from firebase import firebase
-import firebase as fb
 
-URL = 'https://stocks-c6f94.appspot.com/'
+os.environ.setdefault('GOOGLE_APPLICATION_CREDENTIALS', './project.json')
+URL = 'stocks-c6f94.appspot.com'
+
+
+
+def get_storage():
+    return storage.Client()
+
+
+def initiate_firebase():
+    fb = firebase.FirebaseApplication('https://{}'.format(URL),
+                                      authentication=None)
+    fb.storage = get_storage()
+    fb.bucket = fb.storage.get_bucket(URL)
+    return fb
 
 
 class FirebaseStorage(object):
     def __init__(self):
-        self.firebase = firebase.FirebaseApplication(URL, authentication=None)
+        self.firebase = initiate_firebase()
 
 
 class TestFirebaseStorage(TestCase):
@@ -17,5 +31,6 @@ class TestFirebaseStorage(TestCase):
 
     def test_upload_file(self):
         filename = 'model.pkl'
-        with open(filename,  'r+') as file:
-            self.storage.firebase.post('/models', file)
+        test_blob = self.storage.firebase.bucket.blob('/')
+        # with open(filename, 'r+') as file:
+        test_blob.upload_from_filename(filename)
